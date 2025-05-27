2bb06db05c29cf1fe727e0b56ed7ef43d7616431
Khalid Hersi <cipherpolfoundation@gmail.com>
2025-05-27 18:15:15 +0100

    export build

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  export default nextConfig
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'export', // enables static export
+  assetPrefix: isProd ? '/quran-memorization/' : '', // match your repo name
+};
+
+export default nextConfig;
