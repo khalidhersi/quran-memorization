16079a0270d0ba5066415e2af431dd9f1a2aa7d8
Khalid Hersi <cipherpolfoundation@gmail.com>
2025-05-28 16:55:24 +0100

    added btns for other methods

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,6 +42,28 @@ export default function LoginPage() {
       console.error("Google login failed:", error)
     }
   }
+  const handleEmailLogin = async () => {
+    try {
+      if (isMobile) {
+        await signInWithEmailAndPassword
+      } else {
+        await signInWithEmailAndPassword
+      }
+    } catch (error) {
+      console.error("Google login failed:", error)
+    }
+  }
+  const handleAnonymouslyLogin = async () => {
+    try {
+      if (isMobile) {
+        await signInAnonymously
+      } else {
+        await signInAnonymously
+      }
+    } catch (error) {
+      console.error("Google login failed:", error)
+    }
+  }
   
   
 
@@ -51,6 +73,20 @@ export default function LoginPage() {
         <h1 className="text-2xl font-bold mb-4 text-gray-800">Welcome to Quran Memory</h1>
         <p className="mb-6 text-gray-600">Please sign in with your Google account to continue</p>
 
+        <button
+          onClick={handleAnonymouslyLogin}
+          className="flex items-center justify-center gap-3 w-full px-6 py-3 rounded-md bg-blue-600 text-white font-medium shadow hover:bg-blue-700 transition"
+        >
+          <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="h-5 w-5" />
+          Sign in with anonymous
+        </button>
+        <button
+          onClick={handleEmailLogin}
+          className="flex items-center justify-center gap-3 w-full px-6 py-3 rounded-md bg-blue-600 text-white font-medium shadow hover:bg-blue-700 transition"
+        >
+          <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="h-5 w-5" />
+          Sign in with email
+        </button>
         <button
           onClick={handleGoogleLogin}
           className="flex items-center justify-center gap-3 w-full px-6 py-3 rounded-md bg-blue-600 text-white font-medium shadow hover:bg-blue-700 transition"
