e1e2b3422c7a924541af2e1bb2aab4c225448404
Khalid Hersi <cipherpolfoundation@gmail.com>
2025-05-29 10:15:59 +0100

    protected path chnaged test

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -12,7 +12,7 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
 
   useEffect(() => {
     if (!loading && !user && pathname !== "/login") {
-      router.push("/login")
+      router.push("/")
     }
   }, [user, pathname, loading])
 
