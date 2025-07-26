15fe9ff0652046b82a0b7080f944dbe5ba443d1f
Khalid Hersi <cipherpolfoundation@gmail.com>
2025-07-26 17:26:57 +0100

    azure crendentials not linked yet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,30 @@
-resource "azurerm_container_app" "app" {
-  name                         = "quran-memorization-app"
-  container_app_environment_id = azurerm_container_app_environment.env.id
+resource "azurerm_container_app" "quran_app" {
+  name                         = "quran-memorization-aca"
   resource_group_name          = azurerm_resource_group.main.name
+  container_app_environment_id = azurerm_container_app_environment.env.id
   revision_mode                = "Single"
 
   template {
     container {
-      name   = "quran-container"
-      image  = "${azurerm_container_registry.acr.login_server}/${var.image_name}"
-      cpu    = 0.5
-      memory = "1.0Gi"
+      name   = "quran"
+      image  = "youracr.azurecr.io/quran-app:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+#       ports {
+#         port     = 3000
+#         protocol = "TCP"
+#       }
+#     }
 
-    #   ports {
-    #     port     = 3000
-    #     protocol = "TCP"
-    #   }
-    }
+#     scale {
+#       min_replicas = 0
+#       max_replicas = 1
+#     }
+   }
 
-    # ingress {
-    #   external_enabled = true
-    #   target_port      = 3000
-    # }
-  }
-
-#   registry {
-#     server   = azurerm_container_registry.acr.login_server
-#     username = azurerm_container_registry.acr.admin_username
-#     password = azurerm_container_registry.acr.admin_password
-#   }
+#   ingress {
+#     external_enabled = true
+#     target_port      = 3000
+#     transport        = "auto"
+   }
 }
